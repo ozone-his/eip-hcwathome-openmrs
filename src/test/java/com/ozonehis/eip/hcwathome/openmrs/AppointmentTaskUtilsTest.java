67db7898334793f4be98485419dfda8ac9916406
wluyima <wyclif@openmrs.org>
2026-08-06 13:17:48 -0500

    OZ-583: Added EventRoutingProcessorTest and AppointmentTaskUtilsTest

---
+++ src/test/java/com/ozonehis/eip/hcwathome/openmrs/AppointmentTaskUtilsTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright © 2021, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.eip.hcwathome.openmrs;
+
+import static com.ozonehis.eip.hcwathome.openmrs.AppointmentTaskUtils.QUERY_ENCOUNTER;
+import static com.ozonehis.eip.hcwathome.openmrs.AppointmentTaskUtils.createOpenMrsEncounter;
+import static com.ozonehis.eip.hcwathome.openmrs.AppointmentsTask.ENC_TYPE_SYSTEM;
+import static com.ozonehis.eip.hcwathome.openmrs.DbUtils.executeQuery;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.hl7.fhir.r4.model.Encounter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openmrs.eip.EIPException;
+
+@ExtendWith(MockitoExtension.class)
+public class AppointmentTaskUtilsTest {
+	
+	private static MockedStatic<DbUtils> mockDbUtils;
+	
+	@Mock
+	private DataSource mockDataSource;
+	
+	@Mock
+	private OpenmrsFhirClient mockOpenmrsClient;
+	
+	@BeforeEach
+	public void setUp() {
+		mockDbUtils = Mockito.mockStatic(DbUtils.class);
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		mockDbUtils.close();
+	}
+	
+	@Test
+	public void createOpenMrsEncounter_ShouldCreateNewEncounterWhenNoneExists() throws Exception {
+		String appointmentUuid = "test-uuid";
+		String encTypeUuid = "enc-type-uuid";
+		String patientUuid = "pat-uuid";
+		String providerUuid = "prov-uuid";
+		Date startDate = new Date();
+		Date endDate = new Date();
+		Map<String, Object> createdEnc = Map.of("encounter_id", 1, "uuid", "enc_uuid");
+		when(executeQuery(QUERY_ENCOUNTER, mockDataSource, List.of(patientUuid, startDate))).thenReturn(List.of())
+		        .thenReturn(List.of(createdEnc));
+		Encounter encounter = new Encounter();
+		
+		Map<String, Object> result = createOpenMrsEncounter(encounter, appointmentUuid, encTypeUuid, patientUuid,
+		    providerUuid, startDate, endDate, mockDataSource, mockOpenmrsClient);
+		
+		verify(mockOpenmrsClient).create(encounter);
+		assertEquals(createdEnc, result);
+		assertEquals(1, encounter.getType().size());
+		assertEquals(1, encounter.getTypeFirstRep().getCoding().size());
+		assertEquals(ENC_TYPE_SYSTEM, encounter.getTypeFirstRep().getCodingFirstRep().getSystem());
+		assertEquals(encTypeUuid, encounter.getTypeFirstRep().getCodingFirstRep().getCode());
+		assertEquals("Patient/" + patientUuid, encounter.getSubject().getReference());
+		assertEquals(1, encounter.getParticipant().size());
+		assertEquals("Practitioner/" + providerUuid, encounter.getParticipantFirstRep().getIndividual().getReference());
+		assertEquals(startDate, encounter.getPeriod().getStart());
+		assertEquals(endDate, encounter.getPeriod().getEnd());
+	}
+	
+	@Test
+	public void createOpenMrsEncounter_ShouldReturnExistingEncounterIfItExists() throws Exception {
+		String appointmentUuid = "uuid-2";
+		String patientUuid = "pat-uuid";
+		Date startDate = new Date();
+		Date endDate = new Date();
+		Map<String, Object> existingEnc = Map.of("encounter_id", 1, "uuid", "enc_uuid");
+		when(executeQuery(QUERY_ENCOUNTER, mockDataSource, List.of(patientUuid, startDate)))
+		        .thenReturn(List.of(existingEnc));
+		Encounter encounter = new Encounter();
+		
+		Map<String, Object> result = createOpenMrsEncounter(encounter, appointmentUuid, "enc-type-uuid", patientUuid,
+		    "prov-uuid", startDate, endDate, mockDataSource, mockOpenmrsClient);
+		
+		verify(mockOpenmrsClient, never()).create(any());
+		assertEquals(existingEnc, result);
+	}
+	
+	@Test
+	public void createOpenMrsEncounter_ShouldThrowExceptionWhenMultipleEncountersExist() throws Exception {
+		String uuid = "tst-uuid";
+		String patientUuid = "pat-uuid";
+		Date startDate = new Date();
+		Map<String, Object> existingEnc = Map.of("encounter_id", 1, "uuid", "enc_uuid");
+		when(executeQuery(QUERY_ENCOUNTER, mockDataSource, List.of(patientUuid, startDate)))
+		        .thenReturn(List.of(existingEnc, existingEnc));
+		Encounter encounter = new Encounter();
+		
+		Exception e = assertThrows(EIPException.class, () -> createOpenMrsEncounter(encounter, uuid, "enc-type-uuid",
+		    patientUuid, "prov-uuid", startDate, null, mockDataSource, mockOpenmrsClient));
+		
+		assertEquals("Found 2 associated to appointment with uuid " + uuid, e.getMessage());
+	}
+	
+}
--- src/test/java/com/ozonehis/eip/hcwathome/openmrs/EventRoutingProcessorTest.java
+++ src/test/java/com/ozonehis/eip/hcwathome/openmrs/EventRoutingProcessorTest.java
@@ -7,13 +7,72 @@
  */
 package com.ozonehis.eip.hcwathome.openmrs;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openmrs.eip.EIPException;
+import org.openmrs.eip.mysql.watcher.Event;
+
+@ExtendWith(MockitoExtension.class)
 public class EventRoutingProcessorTest {
 	
+	@Mock
+	private Exchange mockExchange;
+	
+	@Mock
+	private Message mockMessage;
+	
+	@Mock
+	private AppointmentEventProcessor mockAppointmentProcessor;
+	
+	private EventRoutingProcessor processor;
+	
+	@BeforeEach
+	public void setup() {
+		processor = new EventRoutingProcessor(mockAppointmentProcessor);
+		when(mockExchange.getIn()).thenReturn(mockMessage);
+	}
+	
 	@Test
-	public void test() throws Exception {
-		//TODO	
+	public void process_shouldProcessWithValidEvent() throws Exception {
+		Event event = Mockito.mock(Event.class);
+		when(mockMessage.getBody()).thenReturn(event);
+		when(event.getTableName()).thenReturn("patient_appointment");
+		
+		processor.process(mockExchange);
+		
+		verify(mockAppointmentProcessor).process(event);
+	}
+	
+	@Test
+	public void process_shouldFailForAnInvalidPayload() {
+		final String payload = "InvalidPayload";
+		when(mockMessage.getBody()).thenReturn(payload);
+		
+		EIPException exception = assertThrows(EIPException.class, () -> processor.process(mockExchange));
+		
+		assertEquals("Don't know how to process payload " + payload, exception.getMessage());
+	}
+	
+	@Test
+	public void process_shouldFailForUnsupportedTable() {
+		Event event = Mockito.mock(Event.class);
+		when(mockMessage.getBody()).thenReturn(event);
+		when(event.getTableName()).thenReturn("bad_table");
+		
+		EIPException exception = assertThrows(EIPException.class, () -> processor.process(mockExchange));
+		
+		assertEquals("No processor found for payload " + event, exception.getMessage());
 	}
 	
 }
